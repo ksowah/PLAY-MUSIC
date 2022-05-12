4f2b3fd5cc4005a81108394762d7ba4259754fde
ksowah <sowahkelvin640@gmail.com>
2022-05-12 14:24:17 +0000

    some style change

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
 
 
   return (
-    <div className='w-full absolute flex items-center justify-between p-4 md:pl-16'>
+    <div className='w-full absolute flex items-center justify-between p-4 md:px-16'>
         <div className='flex items-center space-x-2'>
             <Image src='https://ksets.netlify.app/play/logo.svg' alt='' height={40} width={40}/>
             <h3>Sound Wave</h3>
