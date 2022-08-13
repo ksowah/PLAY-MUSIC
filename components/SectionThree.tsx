c8a33d277d42ba5d542876e89aab23fa18983a71
ksowah <sowahkelvin640@gmail.com>
2022-08-13 20:19:36 +0000

    major overhaul

--- components/SectionThree.tsx
+++ components/SectionThree.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
-import { userState } from "../atoms/userAtom"
+import { sessionState, tokenState } from "../atoms/userAtom"
 import axios from "../utils/axios"
 
 
@@ -9,27 +10,12 @@ const SectionThree = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [token, setToken] = useState("")
-  const [session, setSession] = useState(null)
+  const [token, setToken] = useRecoilState(tokenState)
+  const [session, setSession] = useRecoilState(sessionState)
 
-  const [user, setUser] = useRecoilState(userState)
 
-  // const handleToken = () => {
-  //   axios.interceptors.response.use(
-  //     async (config: AxiosRequestConfig) => {
-  //       if (config.headers === undefined) {
-  //         config.headers = {};
-  //       }
-  //      config.headers.authorization = `Bearer ${user}`
-  //       return config 
-  //     },
 
-  //     error => {
-  //       return Promise.reject(error)
-  //     }
-  //   )
-  // }
-
+// Register users
   const Register = async (e: any) => {
     e.preventDefault()
 
@@ -44,7 +30,7 @@ const SectionThree = () => {
         }
       })
       console.log(data)
-      setUser(data.user)
+      setSession(data.user)
       setToken(data.user.token)
       localStorage.setItem("session", "active")
 
@@ -53,6 +39,8 @@ const SectionThree = () => {
     }
   }
 
+
+  // Log users in
   const Login = async (e: any) => {
     e.preventDefault()
 
@@ -66,7 +54,7 @@ const SectionThree = () => {
         },
       })
       console.log(data)
-      setUser(data)
+      setSession(data)
       setToken(data.token)
       localStorage.setItem("session", "active")
       
@@ -76,6 +64,7 @@ const SectionThree = () => {
     }
   }
 
+  // get all the user
   const getAllUsers = async () => {
     try {
       const {data} = await axios({
@@ -85,14 +74,34 @@ const SectionThree = () => {
           Authorization : `Bearer ${token}`
         }
       })
-      console.log(data)
-      alert("All users")
+
+      console.log(session)
+      
       
     } catch (error: any) {
       console.log(error.response.data.message)
     }
   }
 
+
+  const logOut = async () => {
+    try {
+        await axios({
+        url: "logout",
+        method: "GET",
+      })
+
+      setToken("")
+      setSession(null)
+      localStorage.setItem("session", "inactive")
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  // generate a refreshd token
   const refreshToken = async () => {
     try {
       const { data } = await axios({
@@ -100,24 +109,21 @@ const SectionThree = () => {
         method: "GET",
       })
       
-      console.log(data.token)
       setToken(data.token)
+      setSession(data.user)
       
     } catch (error: any) {
       console.log(error.response.data)      
     }
   }
 
-// useEffect(() => {
-//   refreshToken()
-//   if (localStorage.getItem("session") == "active") {
-//     setInterval( refreshToken, 25000)
-//   }
-// }, [])
+useEffect(() => {
+  refreshToken()
+  if (localStorage.getItem("session") === "active") {
+    setInterval( refreshToken, 4 * 60 * 1000) // every 4 mins
+  }
+}, [])
 
-
-
-  
 
 
   return (
@@ -128,7 +134,7 @@ const SectionThree = () => {
 
       <div className="flex-1 flex items-center justify-center">
 
-        <form className="h-[24rem] w-[25rem] bg-black-rgba p-12" onSubmit={Login}>
+        <form className="h-[24rem] w-[25rem] bg-black-rgba p-12" onSubmit={Register}>
             <label htmlFor="name" className="flex flex-col">
               Name:
               <input type="text" className="my-3 rounded bg-transparent py-1 px-4 border outline-none border-[#1762A7]" onChange={(e) => setName(e.target.value)}/>
@@ -144,11 +150,8 @@ const SectionThree = () => {
               <input type="password" className="my-3 rounded bg-transparent py-1 px-4 border outline-none border-[#1762A7]" onChange={(e) => setPassword(e.target.value)}/>
             </label>
 
-            <button className="bg-[#1762A7] py-1 rounded w-full mt-4" onClick={Login}>Join Now</button>
+            <button className="bg-[#1762A7] py-1 rounded w-full mt-4" onClick={Register}>Join Now</button>
         </form>
-
-        <button onClick={getAllUsers}>Get all users</button>
-        
 
       </div>
 
