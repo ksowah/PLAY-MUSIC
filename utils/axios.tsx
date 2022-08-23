0f8340fbb9c685729baa36d869af49c949556215
ksowah <sowahkelvin640@gmail.com>
2022-08-23 22:16:39 +0000

    use deployed version

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://memoriesbackend123.herokuapp.com/",
     withCredentials: true,
 })
