87dfc72ef2331fbd1ffea81f6a6b72fd82e9d36b
ksowah <sowahkelvin640@gmail.com>
2022-08-25 16:24:11 +0000

    change url

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://memoriesbackend123.herokuapp.com/",
+    baseURL: "http://localhost:8000/",
     withCredentials: true,
 })
